9fefa55523ba166bea1090ccc3fec340f540121b
Alejandro Garcia Gallardo <77563527+AlejandroGaGa@users.noreply.github.com>
2021-08-02 16:38:20 -0500

    Cambio de entity a conexión normal

--- LogicadeNegocios/LogicaNego.cs
+++ LogicadeNegocios/LogicaNego.cs
@@ -9,18 +9,18 @@ namespace LogicadeNegocios
 {
     public class LogicaNego
     {
-        PedidosCarniceriaEntities obj = new PedidosCarniceriaEntities();
+        //PedidosCarniceriaEntities obj = new PedidosCarniceriaEntities();
 
-        public string insertarCarnicero(string name, string cel, string email, short anos)
-        {
-            string res = "";
-            if (name != "" && cel != "" && email != "" && anos != 0)
-            {
-                obj.InsertCarn(name, cel, email, anos);
-                res = "Se inserto correctamente";
-            }
-            return res;
+        //public string insertarCarnicero(string name, string cel, string email, short anos)
+        //{
+        //    string res = "";
+        //    if (name != "" && cel != "" && email != "" && anos != 0)
+        //    {
+        //        obj.InsertCarn(name, cel, email, anos);
+        //        res = "Se inserto correctamente";
+        //    }
+        //    return res;
 
-        }
+        //}
     }// fin de logica de negocio
 }// fin de namespace
