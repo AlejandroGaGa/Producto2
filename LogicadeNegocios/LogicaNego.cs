c91d922f23a98921ae473db9aca6acc9ab1d29d1
Alejandro Garcia Gallardo <77563527+AlejandroGaGa@users.noreply.github.com>
2021-08-02 16:16:13 -0500

    Entity framework

--- LogicadeNegocios/LogicaNego.cs
+++ LogicadeNegocios/LogicaNego.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AccesoaDatos;
 
 namespace LogicadeNegocios
 {
     public class LogicaNego
     {
-    }
-}
+        PedidosCarniceriaEntities obj = new PedidosCarniceriaEntities();
+
+        public string insertarCarnicero(string name, string cel, string email, short anos)
+        {
+            string res = "";
+            if (name != "" && cel != "" && email != "" && anos != 0)
+            {
+                obj.InsertCarn(name, cel, email, anos);
+                res = "Se inserto correctamente";
+            }
+            return res;
+
+        }
+    }// fin de logica de negocio
+}// fin de namespace
---
+++ WebPresentacion/Presentacion.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LogicadeNegocios;
+
+namespace WebPresentacion
+{
+    public partial class Presentacion : System.Web.UI.Page
+    {
+        LogicaNego obj = new LogicaNego();
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+          Label1.Text =  obj.insertarCarnicero(TextBox1.Text, TextBox2.Text, TextBox3.Text, Convert.ToInt16(TextBox4.Text));
+        }
+    }
+}
